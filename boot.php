0fee396dd9e42abe87352ea826850697b1083f9f
Markus Staab <markus.staab@redaxo.de>
2019-01-06 21:01:50 +0100

    readable

--- boot.php
+++ boot.php
@@ -2,8 +2,10 @@
 
 require_once 'vendor/autoload.php';
 
-if (rex::isBackend() && rex::getUser() && rex_one_time_password::required()) {
-    if (!rex_one_time_password::verified()) {
-        rex_be_controller::setCurrentPage('2factor_auth_verify');
+if (rex::isBackend() && rex::getUser()) {
+    if (rex_one_time_password::required()) {
+        if (!rex_one_time_password::verified()) {
+            rex_be_controller::setCurrentPage('2factor_auth_verify');
+        }
     }
 }

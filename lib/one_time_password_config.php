0a6d2900c53481f66e080047d6fa3a14fd5f0495
Marcel Kuhmann <der_marcell@hotmail.com>
2020-08-29 08:43:54 +0200

    Entferne Token nach deaktivieren (#22)

--- lib/one_time_password_config.php
+++ lib/one_time_password_config.php
@@ -75,6 +75,7 @@ final class rex_one_time_password_config
     public function disable()
     {
         $this->enabled = false;
+        $this->provisioningUri = null;
 
         return $this->save();
     }

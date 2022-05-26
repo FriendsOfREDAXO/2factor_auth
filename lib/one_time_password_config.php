a8bc7a73239ee54adc4ed44ce3f4403872bb3835
Alexander Walther <github.com@alexplus.de>
2022-05-26 18:13:59 +0200

    setIssuer (#53)

--- lib/one_time_password_config.php
+++ lib/one_time_password_config.php
@@ -54,10 +54,8 @@ final class rex_one_time_password_config
             // create a uri with a random secret
             $otp = TOTP::create();
             // the label rendered in "Google Authenticator" or similar app
-            $label = $user->getLogin() . '@' . rex::getServername() . ' (' . $_SERVER['HTTP_HOST'] . ')';
-            $label = str_replace(':', '_', $label); // colon is forbidden
-            $otp->setLabel($label);
-
+            $otp->setIssuer(str_replace(':', '_', $user->getLogin()));
+            $otp->setLabel(rex::getServer());
             $this->provisioningUri = $otp->getProvisioningUri();
 
             $this->save();

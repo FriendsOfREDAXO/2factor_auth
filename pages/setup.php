06b60414f557ce11e0c398c22926ae7c3a12d312
Alexander Walther <aw@alexplus.de>
2022-05-26 17:37:17 +0200

    include help page

--- pages/setup.php
+++ pages/setup.php
@@ -1,7 +1,5 @@
 <?php
 
-echo rex_view::title($this->i18n('page_setup'), '');
-
 $csrfToken = rex_csrf_token::factory('2factor_auth_setup');
 $func = rex_request('func', 'string');
 
@@ -31,44 +29,48 @@ if ($otp->enabled()) {
     } elseif ($func === 'setup') {
         $config = rex_one_time_password_config::loadFromDb();
         $uri = $config->provisioningUri; ?>
-        <input type="text" value="<?php echo $uri; ?>" id="2fa-uri" readonly>
-        <clipboard-copy for="2fa-uri" class="">
-            Copy
-        </clipboard-copy>
-        <div id="notice" hidden>Copied to clipboard</div>
+<input type="text" value="<?php echo $uri; ?>" id="2fa-uri" readonly>
+<clipboard-copy for="2fa-uri" class="">
+    Copy
+</clipboard-copy>
+<div id="notice" hidden>Copied to clipboard</div>
 
-        <br /><br />
+<br /><br />
 
-        <canvas id="qr-code"></canvas>
+<canvas id="qr-code"></canvas>
 
-        <script src="<?php echo $this->getAssetsUrl('qrious.min.js'); ?>"></script>
-        <script>
-            new QRious({
-                element: document.getElementById("qr-code"),
-                value: document.getElementById("2fa-uri").value,
-                size: 300
-            });
-        </script>
+<script
+    src="<?php echo $this->getAssetsUrl('qrious.min.js'); ?>">
+</script>
+<script>
+    new QRious({
+        element: document.getElementById("qr-code"),
+        value: document.getElementById("2fa-uri").value,
+        size: 300
+    });
+</script>
 
 
-        <style>
-            clipboard-copy {
-                border: 2px solid black;
-                cursor: default;
-            }
-        </style>
-        <script src="<?php echo $this->getAssetsUrl('clipboard-copy-element.js'); ?>"></script>
-        <script>
-            document.addEventListener('copy', function() {
-                const notice = document.getElementById('notice')
-                notice.hidden = false
-                setTimeout(function() {
-                    notice.hidden = true
-                }, 1000)
-            })
-        </script>
+<style>
+    clipboard-copy {
+        border: 2px solid black;
+        cursor: default;
+    }
+</style>
+<script
+    src="<?php echo $this->getAssetsUrl('clipboard-copy-element.js'); ?>">
+</script>
+<script>
+    document.addEventListener('copy', function() {
+        const notice = document.getElementById('notice')
+        notice.hidden = false
+        setTimeout(function() {
+            notice.hidden = true
+        }, 1000)
+    })
+</script>
 
-        <?php
+<?php
 
         echo '<p><a class="btn btn-setup" href="' . rex_url::currentBackendPage(['func' => 'verify'] + $csrfToken->getUrlParams()) . '">' . $this->i18n('2fa_setup_verify') . '</a></p>';
     } elseif ($func === 'verify') {
@@ -87,15 +89,17 @@ if ($otp->enabled()) {
         }
 
         echo $message; ?>
-        <form method="post">
-            <p><?php echo $this->i18n('2fa_verify_headline'); ?></p>
-            <?php echo $csrfToken->getHiddenField(); ?>
-            <input type="hidden" name="page" value="2factor_auth_setup"/>
-            <input type="hidden" name="func" value="verify"/>
-            <input type="text" name="rex_login_otp"/>
-            <input type="submit" value="<?php echo $this->i18n('2fa_verify_action'); ?>" />
-        </form>
-        <?php
+<form method="post">
+    <p><?php echo $this->i18n('2fa_verify_headline'); ?>
+    </p>
+    <?php echo $csrfToken->getHiddenField(); ?>
+    <input type="hidden" name="page" value="2factor_auth_setup" />
+    <input type="hidden" name="func" value="verify" />
+    <input type="text" name="rex_login_otp" />
+    <input type="submit"
+        value="<?php echo $this->i18n('2fa_verify_action'); ?>" />
+</form>
+<?php
     } else {
         throw new Exception('unknown state');
     }

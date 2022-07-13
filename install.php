d632ece02de6ed44ed089cfa3b60460131e833be
Markus Staab <maggus.staab@googlemail.com>
2022-07-13 10:44:58 +0200

    removed duplicated code of rex_one_time_password::install()

--- install.php
+++ install.php
@@ -4,11 +4,4 @@ rex_sql_table::get(rex::getTable('user'))
     ->ensureColumn(new rex_sql_column('one_time_password_config', 'text'), 'revision')
 ->ensure();
 
-$config = rex_string::yamlDecode(rex_file::get(rex_path::coreData("config.yml")));
-
-if (!array_search("2factor_auth", $config['setup_addons'])) {
-    $config['setup_addons'][] = "2factor_auth";
-    dump($config);
-}
-
 rex_one_time_password::install();

FriendsOfREDAXO/2factor_auth

commit be7ffd4fb4dba3e830151ab3c7eb258bea5886e2

add as setup_addon (#47)
getestet

install.php
@@ -3,3 +3,12 @@
 rex_sql_table::get(rex::getTable('user'))
     ->ensureColumn(new rex_sql_column('one_time_password_config', 'text'), 'revision')
 ->ensure();
+
+$config = rex_string::yamlDecode(rex_file::get(rex_path::coreData("config.yml")));
+
+if (!array_search("2factor_auth", $config['setup_addons'])) {
+    $config['setup_addons'][] = "2factor_auth";
+    dump($config);
+}
+
+one_time_password::install();

lib/one_time_password.php
@@ -42,4 +42,16 @@ final class rex_one_time_password
     {
         return rex_one_time_password_config::forCurrentUser()->enabled;
     }
+
+    public static function install() {
+        $config = rex_string::yamlDecode(rex_file::get(rex_path::coreData("config.yml")));
+
+        if (!array_search("2factor_auth", $config['setup_addons'])) {
+            $config['setup_addons'][] = "2factor_auth";
+            dump($config);
+        }
+
+        rex_file::put(rex_path::coreData("config.yml"), rex_string::yamlEncode($config));
+        return;
+    }
 }

commit 5e338730bb84bdbd0d282dea29a507ce8cfd3749

fix exception on install
```
Error thrown with message "Class "one_time_password" not found"

Stacktrace:
#8 Error in /Users/staabm/workspace/redaxo/redaxo/src/addons/2factor_auth/install.php:14
#7 require in /Users/staabm/workspace/redaxo/redaxo/src/core/lib/packages/package.php:280
#6 rex_package:includeFile in /Users/staabm/workspace/redaxo/redaxo/src/core/lib/packages/manager.php:143
#5 rex_package_manager:install in /Users/staabm/workspace/redaxo/redaxo/src/core/lib/packages/addons/manager.php:27
#4 rex_addon_manager:install in /Users/staabm/workspace/redaxo/redaxo/src/core/lib/packages/api_package.php:34
#3 rex_api_package:execute in /Users/staabm/workspace/redaxo/redaxo/src/core/lib/api_function.php:179
#2 rex_api_function:handleCall in /Users/staabm/workspace/redaxo/redaxo/src/core/backend.php:234
#1 require in /Users/staabm/workspace/redaxo/redaxo/src/core/boot.php:143
#0 require in /Users/staabm/workspace/redaxo/redaxo/index.php:9
```

regressed in https://github.com/FriendsOfREDAXO/2factor_auth/pull/47

install.php
@@ -11,4 +11,4 @@ if (!array_search("2factor_auth", $config['setup_addons'])) {
     dump($config);
 }
 
-one_time_password::install();
+rex_one_time_password::install();

lib/one_time_password.php
@@ -51,6 +51,5 @@ final class rex_one_time_password
         }
 
         rex_file::put(rex_path::coreData("config.yml"), rex_string::yamlEncode($config));
-        return;
     }
 }
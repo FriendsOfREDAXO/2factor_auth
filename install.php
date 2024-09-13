fef147d4635dda27d36fc63b8133f4a776194b9f
Jan Kristinus <jan.kristinus@yakamara.de>
2024-09-13 22:21:46 +0200

    Install und Uninstall angepasst (#94)
    - uninstall fehlten Feldlöschungen
    - Install fehlt das Einbinden des 2FA Setups
    
    closes https://github.com/FriendsOfREDAXO/2factor_auth/issues/93

--- install.php
+++ install.php
@@ -2,6 +2,10 @@
 
 use FriendsOfREDAXO\TwoFactorAuth\setup;
 
+if (!class_exists('FriendsOfREDAXO\TwoFactorAuth\setup')) {
+    include_once rex_addon::get('2factor_auth')->getPath('/lib/setup.php');
+}
+
 rex_sql_table::get(rex::getTable('user'))
     ->ensureColumn(new rex_sql_column('one_time_password_config', 'text'), 'revision')
     ->ensureColumn(new rex_sql_column('one_time_password_tries', 'tinyint'), 'one_time_password_config')

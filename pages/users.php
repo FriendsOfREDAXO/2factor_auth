4984d94bb43354ffce5c6201bf76b54780458b7c
Jan Kristinus <jan.kristinus@yakamara.de>
2024-12-12 12:05:30 +0100

    Admin can now deacivate 2FA for soecific Users

--- pages/users.php
+++ pages/users.php
@@ -2,24 +2,22 @@
 
 use FriendsOfREDAXO\TwoFactorAuth\one_time_password_config;
 
+if ('deactivate' == rex_get('func', 'string') && $userid = rex_get('userid', 'int')) {
+    try {
+        $user = rex_user::get($userid);
+        $config = one_time_password_config::forUser($user);
+        $config->disable();
+
+        echo rex_view::success('User ' . $user->getLogin() . ' deactivated');
+    } catch (Exception $e) {
+        echo rex_view::error($e->getMessage());
+    }
+}
+
 $users = rex_sql::factory();
 $users
     ->setTable(rex::getTable('user'))
     ->select();
-
-$userRows = [];
-foreach ($users as $user) {
-    $user = rex_user::fromSql($user);
-    $config = one_time_password_config::forUser($user);
-    $userRows[] = [
-        $user->getId(),
-        $user->getLogin(),
-        $config->enabled ? 'on' : 'off',
-        $config->method,
-        $user->getValue('one_time_password_tries'),
-        $user->getValue('one_time_password_lasttry'),
-    ];
-}
 
 $content = '<table class="table">';
 
@@ -31,20 +29,29 @@ $content .= '<th>status</th>';
 $content .= '<th>method</th>';
 $content .= '<th>tries</th>';
 $content .= '<th>last_try</th>';
+$content .= '<th>actions</th>';
 $content .= '</tr>';
 $content .= '</thead>';
 
 $content .= '<tbody>';
-foreach ($userRows as $row) {
-    $content .= '<tr>';
-    $content .= '<td data-title="id">' . rex_escape($row[0]) . '</td>';
-    $content .= '<td data-title="login">' . rex_escape($row[1]) . '</td>';
-    $content .= '<td data-title="enabled">' . rex_escape($row[2]) . '</td>';
-    $content .= '<td data-title="method">' . rex_escape($row[3]) . '</td>';
-    $content .= '<td data-title="method">' . rex_escape($row[4]) . '</td>';
-    $content .= '<td data-title="method">' . rex_escape($row[5]) . '</td>';
-    $content .= '</tr>';
+
+$trs = [];
+foreach ($users as $user) {
+    $user = rex_user::fromSql($user);
+    $config = one_time_password_config::forUser($user);
+    $trs[] = '<tr>
+                <td data-title="id">' . rex_escape($user->getId()) . '</td>
+                <td data-title="login">' . rex_escape($user->getLogin()) . '</td>
+                <td data-title="enabled">' . rex_escape($config->enabled ? 'on' : 'off') . '</td>
+                <td data-title="method">' . rex_escape($config->method) . '</td>
+                <td data-title="tries">' . rex_escape($user->getValue('one_time_password_tries')) . '</td>
+                <td data-title="last try">' . rex_escape($user->getValue('one_time_password_lasttry')) . '</td>
+                <td data-title="action"><a href="' . rex_url::currentBackendPage(['func' => 'deactivate', 'userid' => $user->getId()]) . '">deactivate</a></td>
+                </tr>';
 }
+
+$content .= implode('', $trs);
+
 $content .= '</tbody>';
 $content .= '</table>';
 
